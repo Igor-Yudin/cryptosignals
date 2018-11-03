ee36fde6e074187045a51d3604d906839f196d39
Igor-Yudin <Igor_Yudin@inbox.ru>
2018-11-03 16:27:30 +0400

    Modal message with email

--- cryptosignals/main_page/static/main_page/js/subscribe_ajax.js
+++ cryptosignals/main_page/static/main_page/js/subscribe_ajax.js
@@ -7,6 +7,11 @@ $(function() {
 
     var formData = $(form).serialize();
 
+    var email = $('#input_email').val();
+
+    if (email == "")
+      return;
+
     $.ajax({
       type: 'POST',
       url: $(form).attr('action'),
@@ -16,6 +21,8 @@ $(function() {
         if (response.is_ok == true) {
           console.log('subscribe');
           var btnSubscribe = $('#btn-subscribe');
+          $('#modal_message').text(email + ", you've subscribed!");
+          $('#input_email').val("");
           $(btnSubscribe.attr('data-target')).modal('show');
         }
       }
